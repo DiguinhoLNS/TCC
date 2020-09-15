dedf088ea578edd2fa7f31c65b592a12ab78f3d3
luiys <43356660+luiys@users.noreply.github.com>
2020-09-15 18:05:47 -0300

    0.1.2

--- src/Register.php
+++ src/Register.php
@@ -1,31 +1,54 @@
 <?php
 
-	session_start();
+session_start();
 
-	$_SESSION['var'] = '1';
+$_SESSION['var'] = '1';
+
 
 ?>
 
 <!DOCTYPE html>
-<html lang = "pt-br">
+<html lang="pt-br">
 
-	<head>
+<head>
 
-		<title> Cadastro </title>
+	<title> Cadastro </title>
 
-		<?php include "include/Head.php"; ?>
+	<?php include "include/Head.php"; ?>
 
-	</head>
+</head>
 
-	<body id = "RegisterPage" class = "LightMode">
+<body id="RegisterPage" class="LightMode">
 
-		<?php
+	<?php
 
-			if($_SESSION["UserRegisterError_G"] == "1"){
+	if (!isset($_SESSION["UserRegisterError_G"])) {
 
-				if($_SESSION["UserRegisterError_1"] == "1"){
+		$_SESSION["UserRegisterError_G"] = 0;
+		$_SESSION["UserRegisterError_1"] = 0;
+		$_SESSION["UserRegisterError_2"] = 0;
+		$_SESSION["UserRegisterError_3"] = 0;
+		$_SESSION["UserRegisterError_4"] = 0;
+		$_SESSION["UserRegisterError_5"] = 0;
+		$_SESSION["UserRegisterError_6"] = 0;
+		$_SESSION["UserRegisterError_8"] = 0;
+	} else {
 
-					echo '
+		$UserRegisterError_G = $_SESSION["UserRegisterError_G"];
+		$UserRegisterError_1 = $_SESSION["UserRegisterError_1"];
+		$UserRegisterError_2 = $_SESSION["UserRegisterError_2"];
+		$UserRegisterError_3 = $_SESSION["UserRegisterError_3"];
+		$UserRegisterError_4 = $_SESSION["UserRegisterError_4"];
+		$UserRegisterError_5 = $_SESSION["UserRegisterError_5"];
+		$UserRegisterError_6 = $_SESSION["UserRegisterError_6"];
+		$UserRegisterError_8 = $_SESSION["UserRegisterError_8"];
+
+
+		if ($UserRegisterError_G == "1") {
+
+			if ($UserRegisterError_1 == "1") {
+
+				echo '
 					
 						<script language = "javascript" type = "text/javascript">
 						
@@ -38,12 +61,11 @@
 						</script>
 					
 					';
+			}
 
-				}
+			if ($UserRegisterError_2 == "1") {
 
-				if($_SESSION["UserRegisterError_2"] == "1"){
-
-					echo '
+				echo '
 					
 						<script language = "javascript" type = "text/javascript">
 						
@@ -56,12 +78,11 @@
 						</script>
 					
 					';
+			}
 
-				}
+			if ($UserRegisterError_3 == "1") {
 
-				if($_SESSION["UserRegisterError_3"] == "1"){
-
-					echo '
+				echo '
 					
 						<script language = "javascript" type = "text/javascript">
 						
@@ -74,12 +95,11 @@
 						</script>
 					
 					';
+			}
 
-				}
+			if ($UserRegisterError_4 == "1") {
 
-				if($_SESSION["UserRegisterError_4"] == "1"){
-
-					echo '
+				echo '
 					
 						<script language = "javascript" type = "text/javascript">
 						
@@ -92,12 +112,11 @@
 						</script>
 					
 					';
+			}
 
-				}
+			if ($UserRegisterError_5 == "1") {
 
-				if($_SESSION["UserRegisterError_5"] == "1"){
-
-					echo '
+				echo '
 					
 						<script language = "javascript" type = "text/javascript">
 						
@@ -110,12 +129,11 @@
 						</script>
 					
 					';
+			}
 
-				}
+			if ($UserRegisterError_6 == "1") {
 
-				if($_SESSION["UserRegisterError_6"] == "1"){
-
-					echo '
+				echo '
 					
 						<script language = "javascript" type = "text/javascript">
 						
@@ -128,12 +146,11 @@
 						</script>
 					
 					';
+			}
 
-				}
+			if ($UserRegisterError_8 == "1") {
 
-				if($_SESSION["UserRegisterError_8"] == "1"){
-
-					echo '
+				echo '
 					
 						<script language = "javascript" type = "text/javascript">
 						
@@ -146,76 +163,75 @@
 						</script>
 					
 					';
+			}
+		}
+	}
 
-				}
+	?>
 
-			}		
-			
-		?>
+	<main id="MainLogin">
 
-		<main id = "MainLogin">
+		<div class="UserForm BS">
 
-			<div class = "UserForm BS">
+			<form method="POST" action="sql/VerificaCadastro.php">
 
-				<form method = "POST" action = "sql/VerificaCadastro.php">
+				<ul class="UserFormContent">
 
-					<ul class = "UserFormContent">
+					<li class="ContentHeader">
+						<h1> Cadastro </h1>
+					</li>
+					<li class="ContentInput">
+						<label for="R_Nome"> Nome </label>
+						<span id="ErrorNome" class="txtError"> Nome inválido </span>
+						<input id="R_Nome" class="UserInputData" type="text" name="nome" required />
+					</li>
+					<li class="ContentInput">
+						<label for="R_Email"> Email </label>
+						<span id="ErrorEmail" class="txtError"> Email inválido </span>
+						<input id="R_Email" class="UserInputData" type="email" name="email" required />
+					</li>
+					<li class="ContentInput">
+						<label for="R_CPF"> CPF </label>
+						<span id="ErrorCPF" class="txtError"> CPF inválido </span>
+						<input id="R_CPF" class="UserInputData" type="number" name="CPF" required />
+					</li>
+					<li class="ContentInput">
+						<label for="R_DataNasc"> Data de Nascimento </label>
+						<span id="ErrorDataNasc" class="txtError"> Data inválida </span>
+						<input id="R_DataNasc" class="UserInputData" type="date" name="data" required />
+					</li>
+					<li class="ContentInput">
+						<label for="R_Telefone"> Telefone </label>
+						<span id="ErrorTelefone" class="txtError"> Telefone inválido </span>
+						<input id="R_Telefone" class="UserInputData" type="number" name="telefone" required />
+					</li>
+					<li class="ContentInput">
+						<label for="R_Celular"> Celular </label>
+						<span id="ErrorCelular" class="txtError"> Celular inválido </span>
+						<input id="R_Celular" class="UserInputData" type="number" name="celular" required />
+					</li>
+					<li class="ContentInput">
+						<label for="R_Endereco"> Endereço </label>
+						<input id="R_Endereco" class="UserInputData" type="text" name="endereco" required />
+					</li>
+					<li class="ContentInput">
+						<label for="R_Senha"> Senha </label>
+						<span id="ErrorSenha" class="txtError"> Senha inválida </span>
+						<input id="R_Senha" class="UserInputData" type="password" name="senha" required />
+					</li>
+					<li class="ContentBottom">
+						<a href="Login.php"> Já possui uma conta? </a>
+						<input class="UserInputSubmit btn" type="submit" value="Cadastrar">
+					</li>
 
-						<li class = "ContentHeader">
-							<h1> Cadastro </h1>
-						</li>
-						<li class = "ContentInput">
-							<label for = "R_Nome"> Nome </label>
-							<span id = "ErrorNome" class = "txtError"> Nome inválido </span>
-							<input id = "R_Nome" class = "UserInputData" type = "text" name = "nome" required />
-						</li>
-						<li class = "ContentInput">
-							<label for = "R_Email"> Email </label>
-							<span id = "ErrorEmail" class = "txtError"> Email inválido </span>
-							<input id = "R_Email" class = "UserInputData" type = "email" name = "email" required />
-						</li>
-						<li class = "ContentInput">
-							<label for = "R_CPF"> CPF </label>
-							<span id = "ErrorCPF" class = "txtError"> CPF inválido </span>
-							<input id = "R_CPF" class = "UserInputData" type = "number" name = "CPF" required />
-						</li>
-						<li class = "ContentInput">
-							<label for = "R_DataNasc"> Data de Nascimento </label>
-							<span id = "ErrorDataNasc" class = "txtError"> Data inválida </span>
-							<input id = "R_DataNasc" class = "UserInputData" type = "date" name = "data" required />
-						</li>
-						<li class = "ContentInput">
-							<label for = "R_Telefone"> Telefone </label>
-							<span id = "ErrorTelefone" class = "txtError"> Telefone inválido </span>
-							<input id = "R_Telefone" class = "UserInputData" type = "number" name = "telefone" required />
-						</li>
-						<li class = "ContentInput">
-							<label for = "R_Celular"> Celular </label>
-							<span id = "ErrorCelular" class = "txtError"> Celular inválido </span>
-							<input id = "R_Celular" class = "UserInputData" type = "number" name = "celular" required />
-						</li>
-						<li class = "ContentInput">
-							<label for = "R_Endereco"> Endereço </label>
-							<input id = "R_Endereco" class = "UserInputData" type = "text" name = "endereco" required />
-						</li>
-						<li class = "ContentInput">
-							<label for = "R_Senha"> Senha </label>
-							<span id = "ErrorSenha" class = "txtError"> Senha inválida </span>
-							<input id = "R_Senha" class = "UserInputData" type = "password" name = "senha" required />
-						</li>
-						<li class = "ContentBottom">
-							<a href = "Login.php"> Já possui uma conta? </a>
-							<input class = "UserInputSubmit btn" type = "submit" value = "Cadastrar">
-						</li>
+				</ul>
 
-					</ul>
+			</form>
 
-				</form>
+		</div>
 
-			</div>
+	</main>
 
-		</main>
-
-	</body>
+</body>
 
 </html>
--- src/sql/VerificaCadastro.php
+++ src/sql/VerificaCadastro.php
@@ -1,287 +1,288 @@
 <?php
 
-    session_start();
+session_start();
 
-    $tipo_verificacao = $_SESSION['var'];
+$tipo_verificacao = $_SESSION['var'];
 
-    date_default_timezone_set('America/Sao_Paulo');
+date_default_timezone_set('America/Sao_Paulo');
 
-    include "ConexaoBD.php";
+include "ConexaoBD.php";
 
-    switch($tipo_verificacao){
+$_SESSION["UserRegisterError_1"] = 0;
+$_SESSION["UserRegisterError_2"] = 0;
+$_SESSION["UserRegisterError_3"] = 0;
+$_SESSION["UserRegisterError_4"] = 0;
+$_SESSION["UserRegisterError_5"] = 0;
+$_SESSION["UserRegisterError_6"] = 0;
+$_SESSION["UserRegisterError_8"] = 0;
+
+switch ($tipo_verificacao) {
 
         // User Register
-        case 1:
-        
-            $cpfome = $_POST["nome"];
-            $email = $_POST["email"];
-            $cpf = $_POST["CPF"];
-            $data = $_POST["data"];
-            $telefone = $_POST["telefone"];
-            $celular = $_POST["celular"];
-            $endereco = $_POST["endereco"];
-            $senha = $_POST["senha"];
+    case 1:
 
-            $erros = 0;
+        $nome = $_POST["nome"];
+        $email = $_POST["email"];
+        $cpf = $_POST["CPF"];
+        $data = $_POST["data"];
+        $telefone = $_POST["telefone"];
+        $celular = $_POST["celular"];
+        $endereco = $_POST["endereco"];
+        $senha = $_POST["senha"];
+        $E1 = 0;
+        $E2 = 0;
+        $E3 = 0;
+        $E4 = 0;
+        $E5 = 0;
+        $E6 = 0;
+        $E8 = 0;
 
-            //VERIFICA NOME MENOS QUE DOIS CARACTERES E SE POSSUI CARACTERES ESPECIAIS
-            if (strlen($cpfome) <= 2) {
+        $erros = 0;
 
-                $erros++;
+        //VERIFICA NOME MENOS QUE DOIS CARACTERES E SE POSSUI CARACTERES ESPECIAIS
+        if (strlen($nome) <= 2) {
 
-                $E1 = "1";
+            $erros++;
 
-            } else {
+            $E1 = "1";
+        } else {
 
-                for ($i = 0; $i < 11; $i++) {
+            for ($i = 0; $i < strlen($nome); $i++) {
 
-                    if (ord($cpfome[$i]) > 32 && ord($cpfome[$i]) < 65 || ord($cpfome[$i]) > 90 && ord($cpfome[$i]) < 96 || ord($cpfome[$i]) > 122 && ord($cpfome[$i]) < 126) {
-                        
-                        $E1 = "1";
+                if (ord($nome[$i]) > 32 && ord($nome[$i]) < 65 || ord($nome[$i]) > 90 && ord($nome[$i]) < 96 || ord($nome[$i]) > 122 && ord($nome[$i]) < 126) {
 
-                        $erros++;
-
-                    }
-
-                }
-
-            }
-
-            //VERIFICA SE JÁ NAO EXISTE UM USUARIO COM MESMO EMAIL OU MESMO CPF
-            $base = mysqli_connect('localhost', 'root', '', 'ape') or die("erro de conexão");
-
-            $regra1 = "SELECT email, CPF FROM user_plataforma where email =  '$email' and CPF = '$cpf'";
-
-            $res = mysqli_query($base, $regra1) or die("Usuario não cadastrado");
-
-            $mostrar = mysqli_fetch_array($res);
-
-            if (strtolower($mostrar['email']) == strtolower($email) || $mostrar['CPF'] == $cpf) {
-
-                //echo "Email ou CPF ja cadastrados";
-
-                $E2 = "1";
-                $E3 = "1";
-                
-                $erros++;
-
-            }
-
-            //VERIFICA SE O CPF É VALIDO
-            $soma1;
-            $soma2;
-            $digitoum;
-            $digitodois;
-            $val = 0;
-
-            if (empty($cpf) || $cpf < 11) {
-
-                $E3 = "1";
-
-                $val++;
-                $erros++;
-
-            } else {
-
-                for ($i = 0; $i < 11; $i++) {
-
-                    if (ord($cpf[$i]) < 48 || ord($cpf[$i]) > 57) {
-
-                        $E3 = "1";
-
-                        $val++;
-                        $erros++;
-
-                    }
-
-                }
-
-            }
-
-            // Definição de cpf válido
-            if ($val == 0) {
-
-                $soma1 = ($cpf[0] * 10) + ($cpf[1] * 9) + ($cpf[2] * 8) + ($cpf[3] * 7) + ($cpf[4] * 6) + ($cpf[5] * 5) + ($cpf[6] * 4) + ($cpf[7] * 3) + ($cpf[8] * 2);
-                $digitoum = 11 - ($soma1 % 11);
-
-                if ($digitoum > 9) {
-
-                    $digitoum = 0;
-
-                }
-
-                $soma2 = ($cpf[0] * 11) + ($cpf[1] * 10) + ($cpf[2] * 9) + ($cpf[3] * 8) + ($cpf[4] * 7) + ($cpf[5] * 6) + ($cpf[6] * 5) + ($cpf[7] * 4) + ($cpf[8] * 3) + ($digitoum * 2);
-                $digitodois = 11 - ($soma2 % 11);
-
-                if ($digitodois > 9) {
-
-                    $digitodois = 0;
-
-                }
-
-                //verificadores
-                if ($digitoum != $cpf[9] || $digitodois != $cpf[10]) {
+                    $E1 = "1";
 
                     $erros++;
+                }
+            }
+        }
+
+        //VERIFICA SE JÁ NAO EXISTE UM USUARIO COM MESMO EMAIL OU MESMO CPF
+        $base = mysqli_connect('localhost', 'root', '', 'ape') or die("erro de conexão");
+
+        $regra1 = "SELECT email, CPF FROM user_plataforma where email =  '$email' and CPF = '$cpf'";
+
+        $res = mysqli_query($base, $regra1) or die("Usuario não cadastrado");
+
+        $mostrar = mysqli_fetch_array($res);
+
+        if (strtolower($mostrar['email']) == strtolower($email) || $mostrar['CPF'] == $cpf) {
+
+            //echo "Email ou CPF ja cadastrados";
+
+            $E2 = "1";
+            $E3 = "1";
+
+            $erros++;
+        }
+
+        //VERIFICA SE O CPF É VALIDO
+        $soma1;
+        $soma2;
+        $digitoum;
+        $digitodois;
+        $val = 0;
+
+        if (empty($cpf) || strlen($cpf) < 11) {
+
+            $E3 = "1";
+
+            $val++;
+            $erros++;
+        } else {
+
+            for ($i = 0; $i < 11; $i++) {
+
+                if (ord($cpf[$i]) < 48 || ord($cpf[$i]) > 57) {
 
                     $E3 = "1";
 
-                } else {
-
-                    //echo "CPF válido<br> $digitoum--$digitodois";
-
+                    $val++;
+                    $erros++;
                 }
+            }
+        }
 
+        // Definição de cpf válido
+        if ($val == 0) {
+
+            $soma1 = ($cpf[0] * 10) + ($cpf[1] * 9) + ($cpf[2] * 8) + ($cpf[3] * 7) + ($cpf[4] * 6) + ($cpf[5] * 5) + ($cpf[6] * 4) + ($cpf[7] * 3) + ($cpf[8] * 2);
+            $digitoum = 11 - ($soma1 % 11);
+
+            if ($digitoum > 9) {
+
+                $digitoum = 0;
             }
 
-            //VERIFICA SE O ANO CADASTRADO É MAIOR IGUAL AO ATUAL
-            list($ano, $mes, $dia) = explode('-', $data);
-            //echo "Mês: $mes; Dia: $dia; Ano: $ano<br />\n";
+            $soma2 = ($cpf[0] * 11) + ($cpf[1] * 10) + ($cpf[2] * 9) + ($cpf[3] * 8) + ($cpf[4] * 7) + ($cpf[5] * 6) + ($cpf[6] * 5) + ($cpf[7] * 4) + ($cpf[8] * 3) + ($digitoum * 2);
+            $digitodois = 11 - ($soma2 % 11);
 
-            if ($ano >= date("Y")) {
+            if ($digitodois > 9) {
 
-                $E4 = "1";
-
-                $erros++;
-
+                $digitodois = 0;
             }
 
-            //VERIFICA SE FOI DIGITADO O DDD JUNTO COM O TELEFONE
-            if (strlen($telefone) != 10) {
-
-                $E5 = "1";
+            //verificadores
+            if ($digitoum != $cpf[9] || $digitodois != $cpf[10]) {
 
                 $erros++;
+
+                $E3 = "1";
+            } else {
+
+                //echo "CPF válido<br> $digitoum--$digitodois";
 
             }
+        }
 
-            //VERIFICA SE FOI DIGITADO O DDD JUNTO COM O CELULAR
-            if (strlen($celular) < 10) {
+        //VERIFICA SE O ANO CADASTRADO É MAIOR IGUAL AO ATUAL
+        list($ano, $mes, $dia) = explode('-', $data);
+        //echo "Mês: $mes; Dia: $dia; Ano: $ano<br />\n";
 
-                $E6 = "1";
+        if ($ano >= date("Y")) {
 
-                $erros++;
+            $E4 = "1";
 
+            $erros++;
+        }
+
+        //VERIFICA SE FOI DIGITADO O DDD JUNTO COM O TELEFONE
+        if (strlen($telefone) != 10) {
+
+            $E5 = "1";
+
+            $erros++;
+        }
+
+        //VERIFICA SE FOI DIGITADO O DDD JUNTO COM O CELULAR
+        if (strlen($celular) < 10) {
+
+            $E6 = "1";
+
+            $erros++;
+        }
+
+        //VERIFICA SE A SENHA POSSUI PELO MENOS UMA MAIUSCULA E UM NUMERO
+        $i = 0;
+        $Mais = 0;
+        $Mins = 0;
+        $Nmrs = 0;
+        $chars = strlen($senha);
+
+        do {
+
+            if (ord($senha[$i]) >= 65 && ord($senha[$i]) <= 90) {
+
+                $Mais++;
+                $i++;
+            } else if (ord($senha[$i]) >= 97 && ord($senha[$i]) <= 122) {
+
+                $Mins++;
+                $i++;
+            } else if (ord($senha[$i]) >= 48 && ord($senha[$i]) <= 57) {
+
+                $Nmrs++;
+                $i++;
+            }
+        } while ($i < $chars);
+
+        if ($Mins != 0 && $Mais != 0 && $Nmrs != 0) {
+            //echo "Senha válida";
+        } else if ($Mins == 0 && $Mais != 0 && $Nmrs != 0) {
+
+            //echo "Senha inválida, coloque pelo menos uma letra minuscula";
+            $E8 = "1";
+            $erros++;
+        } else if ($Mins != 0 && $Mais == 0 && $Nmrs != 0) {
+
+            //echo "Senha inválida, coloque pelo menos uma letra maiuscula";
+            $E8 = "1";
+            $erros++;
+        } else if ($Mins != 0 && $Mais != 0 && $Nmrs == 0) {
+
+            //echo "Senha inválida, coloque pelo menos um numero";
+            $E8 == "1";
+            $erros++;
+        } else if ($Mins == 0 && $Mais == 0 && $Nmrs == 0) {
+
+            //echo "Senha inválida, digite alguma coisa";
+            $E8 = "1";
+            $erros++;
+        } else if ($Mins == 0 && $Mais == 0 && $Nmrs != 0) {
+
+            //echo "Senha inválida, coloque pelo menos uma letra";
+            $E8 = "1";
+            $erros++;
+        } else if ($Mins != 0 && $Mais == 0 && $Nmrs == 0) {
+
+            //echo "Senha inválida, coloque pelo menos uma letra maiuscula e um numero";
+            $E8 = "1";
+            $erros++;
+        } else if ($Mins == 0 && $Mais != 0 && $Nmrs == 0) {
+
+            //echo "Senha inválida, coloque pelo menos uma letra minuscula e um numero";
+            $E8 = "1";
+            $erros++;
+        }
+
+        //Verifica se não houve erro 
+        if ($erros == 0) {
+
+            $_SESSION["UserRegisterError_G"] = "0";
+
+            include 'InsereCadastro.php';
+        } else if ($erros > 0) {
+
+            //setError($E1, $E2, $E3, $E4, $E5, $E6, $E8);
+
+            $_SESSION["UserRegisterError_G"] = "1";
+
+            echo $E1, $E2, $E3, $E4, $E5, $E6, $E8;
+
+            /*function setError($S1, $S2, $S3, $S4, $S5, $S6, $S8)
+            {*/
+
+            if ($E1 == "1") {
+                $_SESSION["UserRegisterError_1"] = "1";
             }
 
-            //VERIFICA SE A SENHA POSSUI PELO MENOS UMA MAIUSCULA E UM NUMERO
-            $i = 0;
-            $Mais = 0;
-            $Mins = 0;
-            $Nmrs = 0;
-            $chars = strlen($senha);
-
-            do {
-
-                if (ord($senha[$i]) >= 65 && ord($senha[$i]) <= 90) {
-
-                    $Mais++;
-                    $i++;
-
-                } else if (ord($senha[$i]) >= 97 && ord($senha[$i]) <= 122) {
-
-                    $Mins++;
-                    $i++;
-
-                } else if (ord($senha[$i]) >= 48 && ord($senha[$i]) <= 57) {
-
-                    $Nmrs++;
-                    $i++;
-
-                }
-
-            } while ($i < $chars);
-            
-            if ($Mins != 0 && $Mais != 0 && $Nmrs != 0) {
-                //echo "Senha válida";
-            } else if ($Mins == 0 && $Mais != 0 && $Nmrs != 0) {
-
-                //echo "Senha inválida, coloque pelo menos uma letra minuscula";
-                $E8 = "1";
-                $erros++;
-
-            } else if ($Mins != 0 && $Mais == 0 && $Nmrs != 0) {
-
-                //echo "Senha inválida, coloque pelo menos uma letra maiuscula";
-                $E8 = "1";
-                $erros++;
-
-            } else if ($Mins != 0 && $Mais != 0 && $Nmrs == 0) {
-
-                //echo "Senha inválida, coloque pelo menos um numero";
-                $E8 =="1";
-                $erros++;
-
-            } else if ($Mins == 0 && $Mais == 0 && $Nmrs == 0) {
-
-                //echo "Senha inválida, digite alguma coisa";
-                $E8 = "1";
-                $erros++;
-
-            } else if ($Mins == 0 && $Mais == 0 && $Nmrs != 0) {
-
-                //echo "Senha inválida, coloque pelo menos uma letra";
-                $E8 = "1";
-                $erros++;
-
-            } else if ($Mins != 0 && $Mais == 0 && $Nmrs == 0) {
-
-                //echo "Senha inválida, coloque pelo menos uma letra maiuscula e um numero";
-                $E8 = "1";
-                $erros++;
-
-            } else if ($Mins == 0 && $Mais != 0 && $Nmrs == 0) {
-
-                //echo "Senha inválida, coloque pelo menos uma letra minuscula e um numero";
-                $E8 = "1";
-                $erros++;
-
+            if ($E2 == "1") {
+                $_SESSION["UserRegisterError_2"] = "1";
             }
 
-            //Verifica se não houve erro 
-            if($erros == 0){
-
-                $_SESSION["UserRegisterError_G"] = "0";
-
-                include 'InsereCadastro.php';
-
-            }else{
-
-                $_SESSION["UserRegisterError_G"] = "1";
-  
-                setError($E1, $E2, $E3, $E4, $E5, $E6, $E8);
-
-                function setError($S1, $S2, $S3, $S4, $S5, $S6, $S8){
-
-                    if($S1 == "1"){ $_SESSION["UserRegisterError_1"] = "1"; }
-            
-                    if($S2 == "1"){ $_SESSION["UserRegisterError_2"] = "1"; }
-            
-                    if($S3 == "1"){ $_SESSION["UserRegisterError_3"] = "1"; }
-            
-                    if($S4 == "1"){ $_SESSION["UserRegisterError_4"] = "1"; }
-            
-                    if($S5 == "1"){ $_SESSION["UserRegisterError_5"] = "1"; }
-            
-                    if($S6 == "1"){ $_SESSION["UserRegisterError_6"] = "1"; }
-            
-                    if($S8 == "1"){ $_SESSION["UserRegisterError_8"] = "1"; }
-            
-                }
-
-                header("Location: ../Register.php");
-
+            if ($E3 == "1") {
+                $_SESSION["UserRegisterError_3"] = "1";
             }
-        
+
+            if ($E4 == "1") {
+                $_SESSION["UserRegisterError_4"] = "1";
+            }
+
+            if ($E5 == "1") {
+                $_SESSION["UserRegisterError_5"] = "1";
+            }
+
+            if ($E6 == "1") {
+                $_SESSION["UserRegisterError_6"] = "1";
+            }
+
+            if ($E8 == "1") {
+                $_SESSION["UserRegisterError_8"] = "1";
+            }
+            //}
+
+            header("Location: ../Register.php");
+        }
+
         break;
 
-        case 2:break;
+    case 2:
+        break;
 
-        case 3:break;
+    case 3:
+        break;
 
-        default:break;
-
-    }
-    
-
-?>
+    default:
+        break;
+}
