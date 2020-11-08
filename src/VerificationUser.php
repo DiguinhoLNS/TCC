0098e9b5e519b628da68c982a8d9aca038d930ac
DiguinhoLNS <rolns002@gmail.com>
2020-11-08 10:06:19 -0300

    0.12.1

--- src/VerificationUser.php
+++ src/VerificationUser.php
@@ -1,156 +1,158 @@
 <?php
 
-session_start();
-date_default_timezone_set('America/Sao_Paulo');
+    session_start();
+    date_default_timezone_set('America/Sao_Paulo');
 
-require_once "mailer/Exception.php";
-require_once "mailer/SMTP.php";
-require_once "mailer/PHPMailer.php";
-require_once "sql/Funcoes.php";
+    require_once "mailer/Exception.php";
+    require_once "mailer/SMTP.php";
+    require_once "mailer/PHPMailer.php";
+    require_once "sql/Funcoes.php";
 
-$func = new Funcoes();
+    $func = new Funcoes();
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
 
-$mail = new PHPMailer();
+    $mail = new PHPMailer();
 
-if (isset($_GET['q'])) {
-    $email = true;
-} else {
-    $email = false;
-}
-
-if (isset($_POST['V'])) {
-    if ($_SESSION['cod'] == $_POST["V_Cod"]) {
-        header("Location: Dashboard.php");
-        setcookie("ULogged", base64_encode("1"), time() + (86400 * 30), "/");
+    if (isset($_GET['q'])) {
+        $email = true;
     } else {
-        //echo $_POST['V_Cod'] . "<br>erro<br>" . $_SESSION['cod'];
-        $erro = true;
+        $email = false;
     }
-}
 
-if ($email && !isset($erro)) {
-
-    try {
-
-        $emailUser = $_SESSION["email"];
-
-        //echo $emailUser;
-
-        $cod = $func->GerarCodigoAcesso();
-
-        $cod = str_split($cod, 6);
-
-        $rand = rand(0, 1);
-
-        $cod = $cod[$rand];
-
-        $_SESSION["cod"] = $cod;
-
-        $mail->SMTPDebug = false;
-        $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';
-        $mail->SMTPAuth = true;
-        $mail->SMTPSecure = 'ssl';
-        $mail->Port = 465;
-        $mail->Username = 'ape.achadoseperdidos@gmail.com';
-        $mail->Password = 'lmrt2020';
-
-        $mail->SetFrom('ape.achadoseperdidos@gmail.com', 'Ape Achados e Perdidos');
-        $mail->addAddress($emailUser);
-
-        $mail->isHTML(true);
-        $mail->Subject = utf8_encode('Codigo de verificacao');
-        $mail->Body = $cod;
-        $mail->AltBody = $cod;
-
-        if ($mail->send()) {
-            //echo "Enviado";
+    if (isset($_POST['V'])) {
+        if ($_SESSION['cod'] =  $_POST["V_Cod"]) {
+            header("Location: Dashboard.php");
+            setcookie("ULogged", base64_encode("1"), time() + (86400 * 30), "/");
         } else {
-            //echo "Falha";
+            //echo $_POST['V_Cod'] . "<br>erro<br>" . $_SESSION['cod'];
+            $erro = true;
         }
-    } catch (Exception $e) {
-        echo "Erro ao enviar mensagem: {$mail->ErrorInfo}";
     }
-}
 
+    if ($email && !isset($erro)) {
 
+        try {
+
+            $emailUser = $_SESSION["email"];
+
+            //echo $emailUser;
+
+            $cod = $func->GerarCodigoAcesso();
+
+            $cod = str_split($cod, 6);
+
+            $rand = rand(0, 1);
+
+            $cod = $cod[$rand];
+
+            $_SESSION["cod"] = $cod;
+
+            $mail->SMTPDebug = false;
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->SMTPSecure = 'ssl';
+            $mail->Port = 465;
+            $mail->Username = 'ape.achadoseperdidos@gmail.com';
+            $mail->Password = 'lmrt2020';
+
+            $mail->SetFrom('ape.achadoseperdidos@gmail.com', 'Ape Achados e Perdidos');
+            $mail->addAddress($emailUser);
+
+            $mail->isHTML(true);
+            $mail->Subject = utf8_encode('Codigo de verificacao');
+            //$mail->MsgHTML(file_get_contents('include/BodyMail.php'));
+            $mail->Body = $cod;
+            $mail->AltBody = $cod;
+
+            if ($mail->send()) {
+                //echo "Enviado";
+            } else {
+                //echo "Falha";
+            }
+        } catch (Exception $e) {
+            echo "Erro ao enviar mensagem: {$mail->ErrorInfo}";
+        }
+    }
 
 ?>
 
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang = "pt-br">
 
-<head>
+    <head>
 
-    <title> Login </title>
+        <title> Verificação </title>
 
-    <?php include "include/Head.php"; ?>
+        <?php include "include/Head.php"; ?>
 
-</head>
+    </head>
 
-<body id="LoginUserPage" class="LightMode">
+    <body id = "LoginUserPage" class = "LightMode">
 
-    <?php
+        <?php
 
-    include "php/Pag.php";
+            include "php/Pag.php";
 
-    StopUserAccess();
+            StopUserAccess();
 
-    if (isset($erro)) {
+            if (isset($erro)) {
 
-        echo '
+                echo '
+                            
+                    <script language = "javascript" type = "text/javascript">
                     
-                <script language = "javascript" type = "text/javascript">
+                        $(document).ready(function(){
+
+                            $("#ErrorCod").css("display", "block");
+
+                        });
+                    
+                    </script>
                 
-                    $(document).ready(function(){
-    
-                        $("#ErrorCod").css("display", "block");
-    
-                    });
-                
-                </script>
+                ';
             
-            ';
-    
-    } 
+            } 
 
-    ?>
+        ?>
 
-    <main id="MainLoginUser" class="MainFormPlatform">
+        <main id = "MainLoginUser" class = "MainFormPlatform">
 
-        <div class="FormPlatform FormVerification BS">
+            <div class = "FormPlatform FormVerification BS">
 
-            <form class="FormData" method="POST" action="VerificationUser.php">
+                <form class = "FormData" method = "POST" action = "VerificationUser.php">
 
-                <ul class="FormPlatformContent">
+                    <ul class = "FormPlatformContent">
 
-                    <li class="ContentHeader">
-                        <h1> Verificação </h1>
-                    </li>
-                    <li class="ContentInput">
-                        <label for="V_Cod"> Código de Verificação </label>
-                        <input name="V_Cod" id="V_Cod" class="UserInputData" type="text" maxlength="12" required />
-                    </li>
-                    <li class="ContentError">
-                        <span id="ErrorCod" class="txtError"> Código incorreto </span>
-                    </li>
-                    <li class="ContentBottom">
-                        <a href="VerificationUser.php?q=456283650135783"> Enviar código novamente </a>
-                        <input class="UserInputSubmit btn" type="submit" value="Verificar" name="V">
-                    </li>
+                        <li class = "ContentHeader">
+                            <h1> Verificação </h1>
+                        </li>
+                        <li class = "ContentInfo">
+                            <h2> Enviamos ao seu email um código de verificação de segurança </h2>
+                        </li>
+                        <li class = "ContentInput">
+                            <label for = "V_Cod"> Código de Verificação </label>
+                            <input name = "V_Cod" id = "V_Cod" class = "UserInputData" type = "text" maxlength = "12" required />
+                        </li>
+                        <li class = "ContentError">
+                            <span id = "ErrorCod" class = "txtError"> Código incorreto </span>
+                        </li>
+                        <li class = "ContentBottom">
+                            <a href = "VerificationUser.php?q=456283650135783"> Enviar código novamente </a>
+                            <input class = "UserInputSubmit btn" type = "submit" value = "Verificar" name = "V">
+                        </li>
 
-                </ul>
+                    </ul>
 
-            </form>
+                </form>
 
-        </div>
+            </div>
 
-    </main>
+        </main>
 
-</body>
+    </body>
 
 </html>
---
+++ src/include/BodyMail.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang = "pt-br">
+
+    <body class = "MailPage">
+
+        <main>
+
+            <h2> Aqui está o seu código de verificação de segurança da sua conta APE </h2>
+            <br/>
+            <h1> 123456 </h1>
+            <br>
+            <p> NÃO RESPONA A ESSE EMAIL </p>
+
+        </main>
+
+    </body>
+
+</html>
