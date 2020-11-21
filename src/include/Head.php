66b7c281ea467fa86ce81dc5a23ed34217852a33
DiguinhoLNS <rolns002@gmail.com>
2020-11-21 15:35:47 -0300

    0.18.6

--- src/include/Head.php
+++ src/include/Head.php
@@ -7,6 +7,8 @@
 <link rel = "stylesheet" href = "https://fonts.googleapis.com/icon?family=Material+Icons"/>
 <link rel = "stylesheet" href = "https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity = "sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin = "anonymous"/>
 
+<link rel = "icon" type = "image/x-icon" href = "media/favicon.png">
+
 <link rel = "stylesheet" type = "text/css" href = "css/Pag.css"/>
 <link rel = "stylesheet" type = "text/css" href = "css/Responsive.css"/>
 
