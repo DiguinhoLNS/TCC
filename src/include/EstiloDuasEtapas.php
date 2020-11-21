29a01e6c8ca115cf9a3067469a754ecc74938655
DiguinhoLNS <rolns002@gmail.com>
2020-11-21 19:33:17 -0300

    0.18.11

--- src/include/EstiloDuasEtapas.php
+++ src/include/EstiloDuasEtapas.php
@@ -1,18 +1,94 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang = "pt-br">
 
-<body class="MailPage">
+    <head>
+    
+        <meta charset = "UTF-8"/>
+        <meta http-equiv = "Content-Language" content = "pt-br"/>
+        <meta name = "viewport" content = "width=device-width, initial-scale=1.0"/>
 
-    <main>
+        <link rel = "stylesheet" href = "https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap"/>
 
-        <h2 style="color: #202124"> Aqui está o seu código de verificação de segurança da sua conta APE </h2>
-        <br />
-        <h1 style="color: #202124"> <?=$cod?> </h1>
-        <br>
-        <p style="color: #5F6368"> NÃO RESPONDA A ESSE EMAIL </p>
+        <style>
 
-    </main>
+            * {
+                box-sizing: border-box;
+                float: left;
 
-</body>
+                font-family: 'Montserrat', Helvetica, sans-serif;
+                font-style: normal;
+                font-weight: 500;
+            }
+
+            html, body {
+                width: 100%;
+            }
+
+            h1, h2, h3, h4 {
+                width: 100%;
+                padding: 0;
+                margin: 0;
+            }
+        
+            h1 {
+                color: #6A50A7;
+                font-size: 40px;
+                text-align: center;
+            }
+
+            h2 {
+                margin-bottom: 30px;
+                color: #5F6368;
+                font-size: 28px;
+                text-align: center;
+            }
+
+            h3 {
+                color: #202124;
+                font-size: 22px;
+                font-weight: bold;
+                text-align: center;
+            }
+
+            .MailMessage {
+                width: 800px;
+                border: 2px solid #6A50A7;
+                border-radius: 30px;
+            }
+
+            .MailContent {
+                width: 100%;
+                padding: 20px 40px;
+            }
+
+            #MailContentHeader {
+                border-radius: 28px 28px 0 0;
+            }
+        
+        </style>
+
+    </head>
+
+    <body class = "MailPage">
+
+        <div class = "MailMessage">
+        
+            <div id = "MailContentHeader" class = "MailContent">
+            
+                <h1> APE </h1>
+
+            </div>
+
+            <div id = "MailContentMessage" class = "MailContent">
+
+                <h2> Aqui está o código de verificação de segurança da sua conta APE </h2>
+            
+                <h3> <?=$cod?> </h3>
+
+            </div>
+
+        </div>
+
+    </body>
 
 </html>
